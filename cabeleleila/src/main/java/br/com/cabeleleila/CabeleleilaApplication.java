34baba156de7d0db5681dda261ce113db79e46a5
Samir Silva <samirsilva030@gmail.com>
2026-05-20 01:16:48 -0300

    arquivos de inicialização do spring e propriedades do projeto

---
+++ cabeleleila/src/main/java/br/com/cabeleleila/CabeleleilaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cabeleleila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeleleilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabeleleilaApplication.class, args);
+	}
+
+}
---
+++ cabeleleila/src/test/java/br/com/cabeleleila/CabeleleilaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cabeleleila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeleleilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
